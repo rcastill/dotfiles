56ecc8dabfc53d6faaff8ca22f7fa485a1978bad
Rodolfo Castillo Mateluna <rodolfocastillomateluna@gmail.com>
2020-12-17 14:20:09 -0300

    Added autolock user service

---
+++ local/bin/autolock.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Only exported variables can be used within the timer's command.
+export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
+
+# Run xidlehook
+xidlehook \
+  `# Don't lock when there's a fullscreen application` \
+  --not-when-fullscreen \
+  `# Don't lock when there's audio playing` \
+  --not-when-audio \
+  `# lock after 5 minutes` \
+  --timer 300 \
+    'lock-screen.sh' \
+    ''

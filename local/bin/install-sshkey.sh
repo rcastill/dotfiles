4c4d88bae450ff69fb5e5a7c9360506dd9bc386b
Rodolfo <rodolfo@fenrir.localdomain>
2021-07-06 22:58:25 -0400

    Added install-sshkey script

---
+++ local/bin/install-sshkey.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sshargs="$@"
+
+if [[ -z "$sshargs" ]]; then
+    >&2 echo "Usage: $0 sshargs"
+fi
+
+ssh "$@" "mkdir -p ~/.ssh; echo '# $(hostname)' >> ~/.ssh/authorized_keys; echo '$(cat ~/.ssh/id_rsa.pub)' >> ~/.ssh/authorized_keys"

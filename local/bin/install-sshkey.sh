3cc15f61ff8f80f50fb85265b336d300e5ca19bb
Rodolfo <rodolfo@fenrir.localdomain>
2021-07-06 23:00:56 -0400

    Exit on bad use

--- local/bin/install-sshkey.sh
+++ local/bin/install-sshkey.sh
@@ -4,6 +4,7 @@ sshargs="$@"
 
 if [[ -z "$sshargs" ]]; then
     >&2 echo "Usage: $0 sshargs"
+    exit 1
 fi
 
 ssh "$@" "mkdir -p ~/.ssh; echo '# $(hostname)' >> ~/.ssh/authorized_keys; echo '$(cat ~/.ssh/id_rsa.pub)' >> ~/.ssh/authorized_keys"
